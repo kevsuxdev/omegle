ed5e2e2608419a8e64a0d870cec7bc602fa9f514
kevsuxdev <markkevinromero.work@gmail.com>
2025-03-05 22:28:28 +0800

    Republish

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
-import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: process.env.CLIENT_URL,
     methods: ['GET', 'POST'],
   },
 })
