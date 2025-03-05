e6fa47880ee0ab0c1e4d65482ba1758a31fbac50
kevsuxdev <markkevinromero.work@gmail.com>
2025-03-05 22:17:42 +0800

    Change local env variables

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 3000,
+    port: process.env.VITE_FRONTEND_PORT,
   },
 })
